cf736f5c4d43db123b07a7f3aabac2aeaed5fb70
TsaiHao <zaijun521@gmail.com>
2020-11-01 15:28:03 +0800

    move test target to top cmakelists

--- test/source/main.cpp
+++ test/source/main.cpp
@@ -8,7 +8,7 @@ int main()
     cout << "begin" << endl;
     auto f = bind(vrEegConvertAllInFolder, placeholders::_1, placeholders::_2);
     const char* indir = "E:\\Haozaijun\\ExperimentData\\Mengjies\\exp2\\data\\1\\off\\1";
-    const char* outdir = ".\\test";
+    const char* outdir = "test";
     f(indir, outdir);
     return 0;
 }
