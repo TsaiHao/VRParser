163f0495f998db373ff40deb9ed0476ddc062fc3
TsaiHao <zaijun521@gmail.com>
2020-12-14 23:27:20 +0800

    update matlab function

--- parser/wrappers/matlab/ReadPlainFloatToArray.m
+++ parser/wrappers/matlab/ReadPlainFloatToArray.m
@@ -3,19 +3,28 @@ function [Data] = ReadPlainFloatToArray(InputFile, DataType)
 % Read Emg data : emg = ReadPlainFloatToArray('emg.bin', 'Emg');
 
 f = fopen(InputFile);
+if (f < 0)
+    error('input file name is invalid')
+end
 tp = 'single';
-if strcmp(DataType, 'FT')
+DataType = lower(DataType);
+
+if nargin < 2
+    chans = 1;
+elseif strcmp(DataType, 'ft')
     chans = 6;
-elseif strcmp(DataType, 'Emg')
+elseif strcmp(DataType, 'emg')
     chans = 16;
-elseif strcmp(DataType, 'Eeg')
+elseif strcmp(DataType, 'eeg')
     chans = 35;
 else
-    return;
+    error('data type is not defined')
 end
+
 data = fread(f, Inf, tp);
 cols = floor(length(data) / chans);
 Data = reshape(data(1: cols * chans), chans, cols);
 fclose(f);
+
 end
 
